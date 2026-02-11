3383b5558d41c40dd000b988341a24f6b7c5bf35
gautham-inv <gautham.muraleedharan@innovinlabs.com>
2026-02-11 23:29:54 +0530

    fix:changed the cors url

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://ariba-server.onrender.com'],
+    origin: ['https://ariba-client.onrender.com'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
